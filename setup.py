b748006cea9edec3bc584879fa8f0c4096b912a9
cheberamos <cheberamos@gmail.com>
2022-09-22 20:41:44 -0300

    Proyecto Django

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup (
+    name="recursos humanos",
+    version="0.1",
+    description="Paquete de ejemplo de empleados y nomina",
+    author="Cramos",
+    author_email="cheberamos@gmail.com",
+    url="https://codoacodo.com",
+    package= ['recursos_humanos','recursos_humanos.personal'],
+    scripts=[]
+)
